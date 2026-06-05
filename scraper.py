bd4b644ffb7f4808588093c2c23a4a84a614c1c1
Krzysztof W <waszur20@gmail.com>
2026-06-05 19:59:31 +0200

    added main function with GET

--- scraper.py
+++ scraper.py
@@ -1,2 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+
+def main():
+    url = "https://realpython.github.io/fake-jobs/"
+    response = requests.get(url)
+    print(f"Status code: {response.status_code}")
+    html_content = response.text
+    print("\n--- Piece of the HTML page: ---")
+    print(html_content[:500])
+
+if __name__ == "__main__":
+    main()
