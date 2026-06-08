38898453e6af7e39510dc78053e571bf0d00fa47
Krzysztof W <waszur20@gmail.com>
2026-06-08 19:46:18 +0200

    fetch and save posts in db from API

--- api_client.py
+++ api_client.py
@@ -1,5 +1,5 @@
 import requests
-from db_example import add_user, init_database
+from db_example import add_user, init_database, add_post
 
 def main():
  init_database()
@@ -25,6 +25,31 @@ def main():
  except requests.exceptions.HTTPError as err:
     print(f"HTTP Error: Something goes wrong! Detail: {err}")
 
+def fetch_and_save_posts():
+   init_database()
+
+   try:
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+
+    if not data:
+        print("Server response but no data was fetch!")
+    else:
+        for post in data:
+            post_title = post["title"]
+            post_body = post["body"]
+            post_user_id = post["userId"]
+            
+            add_post(post_title, post_body, post_user_id)
+   except requests.exceptions.ConnectionError:
+    print("Error: Can't connect to the server!")
+   except requests.exceptions.HTTPError as err:
+    print(f"HTTP Error: Something goes wrong! Detail: {err}")
+
+
 
 if __name__ == "__main__":
     main()
+    fetch_and_save_posts()
--- db_example.py
+++ db_example.py
@@ -8,6 +8,10 @@ def init_database():
         "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, company TEXT NOT NULL)"
     )
 
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, body TEXT NOT NULL, user_id INTEGER, FOREIGN KEY (user_id) REFERENCES users (id))"
+    )
+
     conn.commit()
     conn.close()
     print("Database and table was successfully created!")
@@ -23,7 +27,19 @@ def add_user(name, company):
     conn.commit()
     conn.close()
 
-def get_all_users():
+
+def add_post(title, body, user_id):
+    conn = sqlite3.connect("my_database.db")
+    cursor = conn.cursor()
+
+    query = "INSERT INTO posts (title, body, user_id) VALUES (?, ?, ?)"
+    values = (title, body, user_id)
+
+    cursor.execute(query, values)
+    conn.commit()
+    conn.close()
+
+def get_all_users():    
     conn = sqlite3.connect("my_database.db")
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM users")
@@ -34,6 +50,31 @@ def get_all_users():
 
     conn.close()
 
+def get_all_posts():
+    conn = sqlite3.connect("my_database.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM posts")
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    conn.close()
+
+def get_users_with_posts():
+    conn = sqlite3.connect("my_database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT users.name, posts.title FROM posts INNER JOIN users ON posts.user_id = users.id LIMIT 10")
+    rows = cursor.fetchall()
+    print("\n=== REPORT: USERS AND THEIR POSTS ===")
+    for row in rows:
+        print(f"Author: {row[0]} | Title: {row[1]}")
+
+    conn.close()
+
 if __name__ == "__main__":
     # init_database()
-    get_all_users()
+    # get_all_users()
+    # get_all_posts()
+    get_users_with_posts()
